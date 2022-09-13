6c16246df32aaf8ce345aceb12cf484b0449ab55
María Elena Álvarez Molina <nutri.mariaelenaalvarez@outlook.com>
2022-09-13 00:17:07 -0400

    Vista Previa de Video

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+function play_video() {
+    var run_video = document.querySelector('.video');
+    run_video.play();
+}
+
+function pause_video() {
+    var stop_video = document.querySelector('.video');
+    stop_video.pause();
+}
